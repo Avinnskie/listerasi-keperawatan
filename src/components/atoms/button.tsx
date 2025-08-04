d1f555b420d436f66737488b8195a557eb89b7c7
Avinnskie <anjasmoonton@gmail.com>
2025-08-04 14:20:42 +0700

    feat: update button type props

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -6,13 +6,13 @@ type ButtonProps = {
 };
 
 export const Button = ({ children, onClick, variant = 'primary', className = '' }: ButtonProps) => {
-  const baseStyle = 'rounded-full px-6 py-2 font-semibold';
+  const baseStyle = 'rounded-md px-6 py-2 font-semibold';
   const variants = {
-    primary: 'bg-green-500 text-white hover:bg-green-600',
+    primary: 'bg-[#38E078] text-white hover:bg-green-600',
     secondary: 'bg-gray-200 text-black hover:bg-gray-300',
   };
   return (
-    <button onClick={onClick} className={`${baseStyle} ${variants[variant]} ${className}`}>
+    <button onClick={onClick} className={`${baseStyle} ${variants[variant]} ${className} w-max`}>
       {children}
     </button>
   );
