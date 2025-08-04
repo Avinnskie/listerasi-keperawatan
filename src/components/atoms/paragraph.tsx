993a310e0594933f206d6398159106e33fab4e6d
Avinnskie <anjasmoonton@gmail.com>
2025-08-04 14:22:01 +0700

    feat: update paragraph type props

--- src/components/atoms/paragraph.tsx
+++ src/components/atoms/paragraph.tsx
@@ -1,3 +1,10 @@
-export const Paragraph = ({ children }: { children: React.ReactNode }) => (
-  <p className="text-lg text-gray-700 text-center max-w-xl mx-auto mt-4">{children}</p>
+type paragraphProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+export const Paragraph = ({ className = '', children }: paragraphProps) => (
+  <p className={`${className} text-lg text-gray-700 text-center max-w-3xl mx-auto mt-4`}>
+    {children}
+  </p>
 );
