8f246bf9d37a10a8ec737a501f075231e241b5c9
Avinnskie <anjasmoonton@gmail.com>
2025-08-05 19:24:54 +0700

    style: delete import and components navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { NavigationBar } from '@/components/molecules/navigationBar';
 import { LandingHero } from '@/components/organisms/landingPage';
 
 export default function Home() {
   return (
     <div className="relative min-h-screen overflow-y-hidden">
-      <NavigationBar className="fixed bg-[#fafcf7] z-50"></NavigationBar>
       <LandingHero />
     </div>
   );
